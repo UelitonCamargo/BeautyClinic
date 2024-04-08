7b0752c4fda2fffdc12a1df7bb5cc12a324783da
Ueliton Camargo <uelitonn6@gmail.com>
2024-04-08 09:42:44 -0300

    Estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Header } from "./components/Header";
+
 export function App() {
   return (
     <>
-      <button>teste</button>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { List } from "@phosphor-icons/react";
+
+export function Header() {
+    return (
+        <header>
+            <h3>Beauty<span>clinic</span>.</h3>
+            <List />
+        </header>
+    )
+}
