d0a1934ff6a45d4b3af9a0beaa7dbd1f09e9f9c9
Ueliton Camargo <uelitonn6@gmail.com>
2024-04-09 10:28:57 -0300

    Adicionado Modal para menu no Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
 
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root') //Acessibilidade
+
 export function App() {
+  const [ isNewMenuModalOpen, setIsNewMenuModalOpen ] = useState(false)
+
+  function handleOpenNewMenu() {
+      setIsNewMenuModalOpen(true)
+  }
+
+  function handleCloseNewMenu() {
+      setIsNewMenuModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewMenu={ handleOpenNewMenu }/>
+
+      <Modal 
+        isOpen={ isNewMenuModalOpen }
+        onRequestClose={ handleCloseNewMenu }
+      >
+               <h1>oiii</h1> 
+      </Modal>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import { List } from "@phosphor-icons/react";
+import styled from './Header.module.css'
 
-export function Header() {
+export function Header({ onOpenNewMenu }) {
     return (
-        <header>
-            <h3>Beauty<span>clinic</span>.</h3>
-            <List />
+        <header className={ styled.containerHeader }>
+            <h3>Beauty<span className={ styled.colorClinic }>clinic</span>.</h3>
+            <button 
+                onClick={ onOpenNewMenu }
+                className={ styled.buttonMenuMobile }
+            >
+                <List />
+            </button>
         </header>
     )
 }
