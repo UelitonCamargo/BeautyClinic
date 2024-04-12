b69ab823f484cfeb14b0577f6293825ef349da9e
Ueliton Camargo <uelitonn6@gmail.com>
2024-04-12 10:47:56 -0300

    Responsividade do Header finalizado!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,15 @@ export function Header({ onOpenNewMenu }) {
     return (
         <header className={ styled.containerHeader }>
             <h3>Beauty<span className={ styled.colorClinic }>clinic</span>.</h3>
+            <nav className={ styled.menuDesktop }>
+                <ul>
+                    <li><a href="">Início</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Depoimentos</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
             <button 
                 onClick={ onOpenNewMenu }
                 className={ styled.buttonMenuMobile }
